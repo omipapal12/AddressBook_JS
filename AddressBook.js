f74f9ad7290e39a17861853c094ef98dbd814a66
omipapal12 <omipapal12@gmail.com>
2021-08-30 16:22:49 +0530

    Added: Search Person By City Or State

--- AddressBook.js
+++ AddressBook.js
@@ -188,10 +188,23 @@ function deleteContact() {
         console.log("Contact is deleleted from Addressbook.")
     }
 }
+/**
+* Ability to search Person in a particular City or State 
+*/
+function searchByCityOrState(searchCityOrState, choice){
+    let contacts = new Array();
+    if(choice == 1){
+        contacts = addressBookArray.filter(contact => contact.city === searchCityOrState)
+    }
+    if(choice == 2){
+        contacts = addressBookArray.filter(contact => contact.state === searchCityOrState)
+    }
+    console.log("Contact: ",contacts);
+}
 
 let choice = 0;
 do {
-    console.log("Press: \n1) Add Contact \n2) Edit Contact \n3) View Contact \n4)Delete Contact\n5)Number Of Contacts\n6)Exit:");
+    console.log("Press: \n1) Add Contact \n2) Edit Contact \n3) View Contact \n4)Delete Contact\n5)Number Of Contacts\n6)Search person by city or state\n7)Exit:");
     choice = Number(prompt("Enter your choice: "));
     if (choice == 1) {
         addContact();
@@ -213,4 +226,18 @@ do {
     if(choice == 5){
         console.log("Number of Contacts are: "+addressBookArray.reduce(contact=>contact + 1, 0));
     }
-} while (choice != 6);
+    if(choice == 6){
+        console.log("1) Search By City  2) Search By State");
+        //Asking user to enter the choice
+        let ch = Number(prompt("Enter your choice: "));
+        switch (ch){
+            case 1: let city = prompt("Enter the city name: ");
+                    ///calling the function  searchByCityOrState
+                    searchByCityOrState(city, 1);
+                    break;
+            case 2: let state = prompt("Enter the state name: ");
+                    searchByCityOrState(state, 2);
+                    break;
+        }
+    }
+} while (choice != 7);
